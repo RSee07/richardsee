64a737bc18e8589686e939977760760b40fe5a5e
Richard See <richardsee@Hi-2.local>
2018-07-10 13:17:43 +1200

    Added react-router-dom and test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to Richard</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to Richard</h1>
+                    </header>
+
+                    <Link to="/newpage" >Test link</Link>
+                    <Route path="/newpage" component={NewPage} />
+                </div>
+            </BrowserRouter>
         );
     }
 }
+
+const NewPage = () => (
+    <div>
+        <h3>Welcome to the new page. Please select more options</h3>
+        <Link to="/newpage/one" style={{padding: "0 10px"}}>One</Link>
+        <Link to="/newpage/two" style={{padding: "0 10px"}}>Two</Link>
+        <Link to="/newpage/three" style={{padding: "0 10px"}}>Three</Link>
+        <Route
+            path="/newpage/:count"
+            render={({match}) => <div style={{padding: "15px"}}>{match.params.count}</div>}
+        />
+    </div>
+)
 
 export default App;
