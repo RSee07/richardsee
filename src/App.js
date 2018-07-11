381fa0f5883ffd94205710fccfe4deeaeaa80589
Richard See <richardsee@Hi-2.local>
2018-07-12 01:13:52 +1200

    Implement basic styled component on landing page

--- src/App.js
+++ src/App.js
@@ -5,24 +5,45 @@ import {
     NavLink,
     Switch
 } from 'react-router-dom'
+import styled from 'styled-components'
 import Work from '../src/Components/Work'
 import About from '../src/Components/About'
 import Contact from '../src/Components/Contact'
-import './App.css'
+
+const Background = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: inline-block;
+    background-color: #000000;
+
+`
+
+const TitleContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Title = styled.h1`
+    color: #ffffff;
+`
 
 class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
-                <div className="App">
-                    <header className="App-header">
-                    <h1 className="App-title">Hi I'm Richard</h1>
+                <Background>
+                    <header>
+                        <TitleContainer>
+                            <Title>Hi I'm Richard</Title>
+                        </TitleContainer>
+                        <NavLink to="/work" style={{padding: "10px", display: "inline-block"}}>Work</NavLink>
+                        <NavLink to="/about" style={{padding: "10px", display: "inline-block"}}>About</NavLink>
+                        <NavLink to="/contact" style={{padding: "10px", display: "inline-block"}}>Contact</NavLink>
+                        <NavLink to="/testpage" style={{padding: "10px", display: "inline-block"}}>Test link</NavLink>
                     </header>
-
-                    <NavLink to="/work" style={{padding: "10px", display: "inline-block"}}>Work</NavLink>
-                    <NavLink to="/about" style={{padding: "10px", display: "inline-block"}}>About</NavLink>
-                    <NavLink to="/contact" style={{padding: "10px", display: "inline-block"}}>Contact</NavLink>
-                    <NavLink to="/testpage" style={{padding: "10px", display: "inline-block"}}>Test link</NavLink>
 
                     <Switch>
                         <Route path="/work" render={() => <Work work="Work page" />} />
@@ -30,7 +51,7 @@ class App extends React.Component {
                         <Route path="/contact" render={() => <Contact contact="Contact page" />} />
                         <Route path="/testpage" component={TestPage} />
                     </Switch>
-                </div>
+                </Background>
             </BrowserRouter>
         )
     }
@@ -41,7 +62,7 @@ const TestPage = () => (
         <h3>Welcome to the new page. Please select more options</h3>
         <NavLink to="/testpage/one" style={{padding: "0 10px"}}>One</NavLink>
         <NavLink to="/testpage/two" style={{padding: "0 10px"}}>Two</NavLink>
-        <NavLink to="/tetpage/three" style={{padding: "0 10px"}}>Three</NavLink>
+        <NavLink to="/testpage/three" style={{padding: "0 10px"}}>Three</NavLink>
         <Route
             path="/testpage/:count"
             render={({match}) => <div style={{padding: "15px"}}>{match.params.count}</div>}
