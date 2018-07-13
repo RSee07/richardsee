b0568f8a1e01837dc4771b131e33ab1c72622cdf
Richard See <richardsee@Hi-2.local>
2018-07-14 00:15:30 +1200

    Moved Frosted Glass component out

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom'
 import styled from 'styled-components'
 import Header from './components/Header'
-import hero from './static/media/hero.JPG'
+
 import Work from './components/Work'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -15,40 +15,12 @@ import Contact from './components/Contact'
 const Background = styled.div`
     height: 100vh;
     width: 100vw;
-    background-image: url(${hero});
-    background-position: 50% 50%;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    overflow: auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    background-color: #282828;
 `
 
 const TitleWrapper = styled.div`
-    position: relative;
-    background: inherit;
-    border-radius: 10px;
-    overflow: hidden;
-
-    &:after {
-        content: '';
-        background: inherit;
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        box-shadow: inset 0 0 0 200px rgba(255,255,255,0.05);
-        filter: blur(15px);
-    }
-`
-
-const TitleBox = styled.div`
     padding: 15px 40px;
     position: relative;
-    z-index: 1;
 `
 
 const Title = styled.h1`
@@ -81,9 +53,7 @@ class App extends React.Component {
                     <Background>
                         <Header />
                         <TitleWrapper>
-                            <TitleBox>
-                                <Title>Richard See</Title>
-                            </TitleBox>
+                            <Title>RICHARD SEE</Title>
                         </TitleWrapper>
                     </Background>
                     <div style={{height: '700px'}}>
---
+++ src/components/FrostedGlassExample.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import hero from './static/media/hero.JPG'
+
+const Background = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background-image: url(${hero});
+    background-position: 50% 50%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    overflow: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const TitleWrapper = styled.div`
+    position: relative;
+    background: inherit;
+    border-radius: 10px;
+    overflow: hidden;
+
+    &:after {
+        content: '';
+        background: inherit;
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        box-shadow: inset 0 0 0 200px rgba(255,255,255,0.05);
+        filter: blur(10px);
+    }
+`
+
+const TitleBox = styled.div`
+    padding: 15px 40px;
+    position: relative;
+    z-index: 1;
+`
+
+const Title = styled.h1`
+    color: #ffffff;
+    margin: 0;
+    font-size: 50px;
+`
+
+const FrostGlassExample = () => (
+    <Background>
+        <TitleWrapper>
+            <TitleBox>
+                <Title>Example</Title>
+            </TitleBox>
+        </TitleWrapper>
+    </Background>
+)
+
+export default FrostGlassExample
