73ec926aa2655301a864986cda3dd52fe73371b8
Richard See <richardsee@Hi-2.local>
2018-07-26 00:57:01 +1200

    Added circle around footer and positioned correctly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,25 @@ import styled from 'styled-components'
 
 const FooterWrapper = styled.footer`
     display: inline-block;
-    position: relative;
+    position: fixed;
     z-index: 1;
+    bottom: 15px;
+    right: 15px;
+`
+
+const Circle = styled.div`
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+    background-color: #3C3C3C;
+    box-shadow: 2px 2px 40px #000000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const BarWrapper = styled.div`
+    margin-bottom: 1px;
 `
 
 const Bar1 = styled.div`
@@ -12,11 +29,12 @@ const Bar1 = styled.div`
     height: 5px;
     background-color: #ffffff;
     transition: 0.4s;
+    margin: auto;
     transform: ${prop => prop.open ? 'rotate(-45deg) translate(-8px, 7.5px)' : undefined};
 `
 
 const Bar2 = Bar1.extend`
-    margin: 6px 0;
+    margin: 6px auto;
     transform: none;
     opacity: ${prop => prop.open ? '0' : '1'};
 `
@@ -57,9 +75,13 @@ export default class Footer extends React.Component {
     render() {
         return(
             <FooterWrapper>
-                <Bar1 open={this.state.openDropdown}></Bar1>
-                <Bar2 open={this.state.openDropdown}></Bar2>
-                <Bar3 open={this.state.openDropdown}></Bar3>
+                <Circle>
+                    <BarWrapper>
+                        <Bar1 open={this.state.openDropdown}></Bar1>
+                        <Bar2 open={this.state.openDropdown}></Bar2>
+                        <Bar3 open={this.state.openDropdown}></Bar3>
+                    </BarWrapper>
+                </Circle>
                 <DropdownButton onClick={this.toggleDropdown} />
             </FooterWrapper>
         )
